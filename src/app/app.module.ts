3bc6405fbea4f38d3e4de9be0387277ec775d0f8
saharejaz2324 <sej@ciklum.com>
2021-06-15 21:29:50 +0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,17 @@ import { IntervalComponent } from './observable/interval/interval.component';
 import { OperatorComponent } from './observable/operator/operator.component';
 import { MouseEventComponent } from './observable/mouse-event/mouse-event.component';
 import { HttpClientComponent } from './observable/http-client/http-client.component';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ApiCountryCallService } from './services/api-country-call.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, NgbModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
   declarations: [
     AppComponent,
     HeaderComponentComponent,
--- src/app/services/api-country-call.service.ts
+++ src/app/services/api-country-call.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
@@ -12,12 +12,12 @@ export class ApiCountryCallService {
   responseData: any;
 
   searchCountryByItsName(countryName: string): Observable<Country[]> {
-    let headers: HttpHeaders = new HttpHeaders();
-    headers = headers.append('Accept', 'application/json');
-    headers = headers.append(
-      'X-RapidAPI-Key',
-      '1108554cc1mshf11c17c4fea2b3dp179054jsn2446fb7a8965'
-    );
+    // let headers: HttpHeaders = new HttpHeaders();
+    // headers = headers.append('Accept', 'application/json');
+    // headers = headers.append(
+    //   'X-RapidAPI-Key',
+    //   '1108554cc1mshf11c17c4fea2b3dp179054jsn2446fb7a8965'
+    // );
     // this.httpResponse = this.httpClient.get('https://restcountries-v1.p.rapidapi.com/capital/' + countryName, {headers: headers});
     this.httpResponse = this.httpClient.get(
       'https://restcountries.eu/rest/v2/name/' + countryName
